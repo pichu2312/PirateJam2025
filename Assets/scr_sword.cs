179df509a8661cc58264dce0e2a1c5c85ee91af1
Jacob <JacobBradford2312@Outlook.com>
2025-01-18 22:27:48 +0000

    Twanging
    Can now hold S to make it pull back and move

--- Assets/scr_sword.cs
+++ Assets/scr_sword.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using TMPro;
 using UnityEngine;
+using UnityEngine.WSA;
 
 
 
@@ -18,12 +21,35 @@ public class scr_sword : MonoBehaviour
 
     [SerializeField]
     private Quaternion tpRotation = new Quaternion(30, 0, -180, 0);
+    public TextMeshProUGUI debugText; 
 
 
     //Components
-    public CapsuleCollider hiltHitbox;
-    public CapsuleCollider swordHitbox;
-    public CapsuleCollider pointHitbox;
+    private CapsuleCollider hiltHitbox;
+    private CapsuleCollider swordHitbox;
+    private CapsuleCollider pointHitbox;
+
+
+    private Rigidbody rigidbody;
+    private Camera camera;
+    private Quaternion baseCamRot;
+    private SpriteRenderer arrow;
+
+    //Collision
+    [SerializeField, Range(0.005f, 0.1f)]
+    public float swordSharpness = 1.0f;
+    private float swordTimer = 0f;
+    private bool swordEnteringTerrain;
+
+    //Launching
+    private bool launchCharging;
+    private float launchTimer = 0;
+    public float minLaunch = 1f;
+    public float maxLaunch = 3f;
+    
+
+    //Controls
+    private KeyCode pullBack = KeyCode.S;
 
 
     // Start is called before the first frame update
@@ -40,19 +66,94 @@ public class scr_sword : MonoBehaviour
         hiltHitbox = cylinders[0];
         swordHitbox = cylinders[1];
         pointHitbox = cylinders[2];
+
+        rigidbody = GetComponent<Rigidbody>();
+
+        camera = GetComponentInChildren<Camera>();
+        baseCamRot = camera.transform.rotation;
+
+        arrow = GetComponentInChildren<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         DebugCommands();
+
+        UserInput();
+
+        ProcessLaunchCharging();
+    }
+    void FixedUpdate()
+    {
+        ProcessCollision();
+    }
+
+    void LateUpdate() {
+        camera.transform.rotation = baseCamRot;
     }
 
     void DebugCommands() {
         if (debug) {
             if (Input.GetKeyUp(KeyCode.Tab)) {
-                transform.position = tpPosition;
-                transform.rotation = tpRotation;
+                transform.SetPositionAndRotation(tpPosition, tpRotation);
+            }
+
+            if (Input.GetKeyUp(KeyCode.Return)) {
+                swordHitbox.enabled = true;
+            }
+
+            debugText.text = "Velocity: " + rigidbody.velocity;
+
+        }
+    }
+
+    void UserInput() {
+        if (Input.GetKeyDown(pullBack)) {
+            arrow.enabled = true;
+            launchCharging = true;
+            launchTimer = 0;
+        }
+
+
+        if (Input.GetKeyUp(pullBack)) {
+            arrow.enabled = false;
+            launchCharging = false;
+
+            if (launchTimer > minLaunch) {
+                Launch();
+            }
+        }
+    }
+
+    void ProcessLaunchCharging() {
+        if (launchCharging) {
+            launchTimer += Time.deltaTime;
+
+
+            //POTENTIALLY INEFFICENT
+            if (launchTimer > maxLaunch) {
+                launchTimer = maxLaunch;
+            }
+
+
+            float arrowLength = Mathf.Lerp(0.05f, 0.3f, launchTimer/maxLaunch);
+            arrow.transform.localScale = new Vector3(arrow.transform.localScale.x, arrowLength,0);
+        }
+    }
+
+    //If the sword is entering a piece of terrain, increase the timer before its hitbox is renabled
+    void ProcessCollision() {
+        if (swordEnteringTerrain) {
+            swordTimer += Time.deltaTime;
+
+            if (swordTimer > swordSharpness) {
+                rigidbody.velocity = Vector3.zero;
+                rigidbody.angularVelocity = Vector3.zero;
+                rigidbody.useGravity = false;
+
+                //wordHitbox.enabled = true;
+                swordEnteringTerrain = false;
             }
         }
     }
@@ -64,6 +165,9 @@ public class scr_sword : MonoBehaviour
 
         //Set the sword hitbox to no longer collide
         swordHitbox.enabled = false;
+        swordEnteringTerrain = true;
+
+        swordTimer = 0f;
     }
 
     void OnTriggerExit(UnityEngine.Collider other)
@@ -71,6 +175,12 @@ public class scr_sword : MonoBehaviour
         Debug.Log("Uncolliding with " + other.name);
 
         swordHitbox.enabled = true;
+        rigidbody.useGravity = true;
 
+
+    }
+
+    void Launch() {
+        rigidbody.AddForce(new Vector3(200 * launchTimer, 100 * launchTimer, 0), ForceMode.Force);
     }
 }
